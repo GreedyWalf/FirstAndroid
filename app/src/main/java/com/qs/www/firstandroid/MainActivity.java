28b743ce86860bb9f0e27f8151a4836aa03b30b7
qinyupeng <695830237@qq.com>
2019-07-04 11:04:31 +0800

    WebView初体验；

--- app/src/main/java/com/qs/www/firstandroid/MainActivity.java
+++ app/src/main/java/com/qs/www/firstandroid/MainActivity.java
@@ -1,49 +1,125 @@
 package com.qs.www.firstandroid;
 
 import android.annotation.SuppressLint;
-import android.content.Intent;
-import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.text.Editable;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+import android.webkit.*;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private WebView webView;
+
+    private ProgressBar progressBar;
+
+    @SuppressLint("JavascriptInterface")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //加载布局
         setContentView(R.layout.activity_main);
 
-        //获取布局中控件
-        final EditText editText = findViewById(R.id.editText);
-        Button button = findViewById(R.id.btnCall);
+        progressBar = findViewById(R.id.progressbar);
+        webView = findViewById(R.id.webview);
+        //webView中加载页面
+        webView.loadUrl("https://www.baidu.com");
 
-        //添加按钮点击监听事件
-        button.setOnClickListener(new View.OnClickListener() {
-            @SuppressLint("ShowToast")
-            @Override
-            public void onClick(View view) {
-                String number = editText.getText().toString().trim();
-                System.out.println("-->>测试一下");
-                if ("".equals(number)) {
-                    Toast.makeText(MainActivity.this, "number不能为空！", Toast.LENGTH_LONG);
-                    return;
-                }
+        //添加js监听，这样html就能调用客户端
+        webView.addJavascriptInterface(this, "android");
+        webView.setWebChromeClient(webChromeClient);
+        webView.setWebViewClient(webViewClient);
 
-                //创建意图对象
-                Intent intent = new Intent();
+        WebSettings webSettings = webView.getSettings();
+        //允许使用js
+        webSettings.setJavaScriptEnabled(true);
+        //不使用缓存
+        webSettings.setCacheMode(WebSettings.LOAD_NO_CACHE);
+        //支持屏幕缩放
+        webSettings.setSupportZoom(true);
+        webSettings.setBuiltInZoomControls(true);
 
-                /**
-                 * uri：统一资源标识符
-                 */
-                intent.setAction(Intent.ACTION_CALL).setData(Uri.parse("tel:" + number));
-                startActivity(intent);
+    }
+
+    //WebViewClient主要帮助WebView处理各种通知、请求事件
+    private WebViewClient webViewClient = new WebViewClient() {
+        //页面加载完成
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            progressBar.setVisibility(View.GONE);
+        }
+
+        //页面开始加载
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            progressBar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            Log.i("ansen", "拦截url:" + url);
+            if (url.equals("http://www.google.com/")) {
+                Toast.makeText(MainActivity.this, "国内不能访问google,拦截该url", Toast.LENGTH_LONG).show();
+                return true;//表示我已经处理过了
             }
-        });
+            return super.shouldOverrideUrlLoading(view, url);
+        }
+
+    };
+
+    /**
+     * WebChromeClient主要辅助WebCiew处理javascript的对话框、网站图标、网站title、加载进度等
+     */
+    private WebChromeClient webChromeClient = new WebChromeClient() {
+        @Override
+        public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
+            AlertDialog.Builder localBuilder = new AlertDialog.Builder(webView.getContext());
+            localBuilder.setMessage(message).setPositiveButton("确定", null);
+            localBuilder.setCancelable(false);
+            localBuilder.create().show();
+
+            //！注意：这句代码必须要加。表示处理结果为确定状态同事唤醒WebCore线程，否则不能继续点击按钮
+            result.confirm();
+            return true;
+        }
+
+        //获取网页标题
+        @Override
+        public void onReceivedTitle(WebView view, String title) {
+            super.onReceivedTitle(view, title);
+            Log.i("ansen", "网页标题:" + title);
+        }
+
+        //加载进度回调
+        @Override
+        public void onProgressChanged(WebView view, int newProgress) {
+            progressBar.setProgress(newProgress);
+        }
+    };
+
+
+    /**
+     * Js调用android的方法
+     *
+     * @param str
+     */
+    @JavascriptInterface
+    public void getClient(String str) {
+        Log.i("ansen", "html调用客户端：" + str);
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        //释放资源
+        webView.destroy();
+        webView = null;
     }
 }
